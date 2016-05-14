0c8c3b1757d12132e0752b6113c8ff6808126931
Dhaval Varsada <dhaval.v@tasolglobal.com>
2016-05-14 16:48:41 +0530

    phpinfo added

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //add comment for  commit file from development repository
 echo "hello World";
 //add comment for  commit file from HotFix repository
-
+echo phpinfo();
 
 
 //add comment for  commit file from HotFix Demo 1  repository
